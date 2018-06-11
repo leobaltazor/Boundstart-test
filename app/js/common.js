6ffe948b85b84670294c9e6c8c2606e7789c59fc
Leo <leo.baltazor@gmail.com>
2018-06-11 15:24:45 +0300

    specialists

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
+  $(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      URLhashListener: true,
+      autoplayHoverPause: true,
+      center: true,
+      startPosition: "functional",
+      autoplay: true,
+    //   onChanged: log,
+      autoplayTimeout: 10000
+    });
+  });
 
-	// Custom JS
-
+  // Custom JS
 });
